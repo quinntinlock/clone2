568074d61d72acc532e82c1d8a8628abff166b4e
Quinn Lockhart <animerq2@gmail.com>
2024-11-17 16:48:49 -0500

    Fixed error in the average function to properly calculate the math as well as the error on line 16 so the program properly accepts the correct data.

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -2,9 +2,9 @@
 #include <iomanip>
 using namespace std;
 
-float average(int i1, int i2, int i3)
+float average(int n1, int n2, int n3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	return (n1 + n2 + n3) / 3.0f;
 }
 
 int main()
@@ -13,7 +13,7 @@ int main()
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3;
 
 	float a = average(n1, n2, n3);
 
